bd37ee3adeed8fa2552f51837f88ebd06183adec
FURB\jdomareski <jefferson.domareski@gmail.com>
2022-10-11 21:44:24 -0300

    Diagrama de classes

---
+++ Programação Orientada a Objeto/Diagrama de classes/Diag1b.dpr
@@ -0,0 +1,15 @@
+program Diag1b;
+
+uses
+  Vcl.Forms,
+  Diag1 in 'Diag1.pas' {Form1},
+  UPessoa in 'UPessoa.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
